4fd5c932ef91be010fc9bfad62570fee35e278c7
Michael C <mwcxpt@umsystem.edu>
2022-02-10 17:27:57 -0600

    Initial

---
+++ matrix_search.cpp
@@ -0,0 +1,45 @@
+/**
+Here in the .cpp you should define and implement the functions that are declared in the .h file.
+It is not recommended to add extra functions on this assignment.
+**/
+
+#include "matrix_search.h"
+
+
+// We're giving you this one the first time, and you should copy it into all future assignments.
+// Change the information to your own email handle (S&T username)
+void get_identity(string &my_id)
+{
+    my_id = "janexc3";
+}
+
+
+char ** build_matrix(int rows, int cols)
+{
+    // Define your function here
+}
+
+
+void fill_matrix(int rows, int cols, char **matrix)
+{
+    // Define your function here
+}
+
+
+void print_matrix(int rows, int cols, char **matrix)
+{
+    // Define your function here
+}
+
+
+void delete_matrix(int rows, char **matrix)
+{
+    // Define your function here
+}
+
+
+void matrix_search(int sol[], string word, int rows, int cols, char **matrix)
+{
+    // Define your function here
+}
+
---
+++ pa01.cpp
@@ -0,0 +1,13 @@
+/**
+Implement the main here to match input and output. 
+**/
+
+#include "matrix_search.h"
+
+int main()
+{
+    // Write your main here.
+
+    return 0;
+}
+
