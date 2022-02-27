117c1b2cafd51616c58cae29b1499149b2364184
Spering <rjstrd@rc30xcs213.managed.mst.edu>
2022-02-27 13:21:56 -0600

    test me pls

--- matrix_search.cpp
+++ matrix_search.cpp
@@ -10,36 +10,98 @@ It is not recommended to add extra functions on this assignment.
 // Change the information to your own email handle (S&T username)
 void get_identity(string &my_id)
 {
-    my_id = "janexc3";
+  my_id = "rjstrd";
 }
 
 
 char ** build_matrix(int rows, int cols)
 {
-    // Define your function here
+  char** matrix = new char*[rows];
+  for(int i = 0; i < rows; i++)
+  {
+    matrix[i] = new char[cols];
+  }
+
+  return matrix;
 }
 
 
 void fill_matrix(int rows, int cols, char **matrix)
 {
-    // Define your function here
+  for(int i = 0; i < rows; i++)
+  {
+    for(int j = 0; j < cols; j++)
+    {
+      cin >> matrix[i][j];
+    }
+  }
 }
 
 
 void print_matrix(int rows, int cols, char **matrix)
 {
-    // Define your function here
+  for(int i = 0; i < rows; i++)
+  {
+    for(int j = 0; j < cols; j++)
+    {
+      cout << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
 }
 
 
 void delete_matrix(int rows, char **matrix)
 {
-    // Define your function here
+  for(int i = 0; i < rows; i++)
+  {  
+    delete[] matrix[i];
+  }
+  delete[] matrix;
 }
 
 
 void matrix_search(int sol[], string word, int rows, int cols, char **matrix)
 {
-    // Define your function here
+  int length = word.length();
+  int xDirections[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+  int yDirections[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+  
+  for(int i = 0; i < 4; i++)
+  {
+    sol[i] = -1;
+  }
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      if (matrix[i][j] == word[0])
+      {
+        for (int d = 0; d < 8; d++)
+        {
+          for(int k = 1; k < length; k++)
+          {
+            if ((i + (k * yDirections[d])) >= 0 && (j + (k * xDirections[d])) >= 0 && matrix[i + (k * yDirections[d])][j + (k * xDirections[d])] == word[k] && (i + (k * yDirections[d])) < rows && (j + (k * xDirections[d])) < cols)
+            {
+              if (k == length - 1)
+              {
+                sol[0] = i;
+                sol[1] = j;
+                sol[2] = i + k * yDirections[d];
+                sol[3] = j + k * xDirections[d];
+              
+              }
+            }
+            else
+            {
+              break;
+            }
+          }
+          
+        }
+      }
+    }
+  }
 }
 
--- pa01.cpp
+++ pa01.cpp
@@ -6,8 +6,43 @@ Implement the main here to match input and output.
 
 int main()
 {
-    // Write your main here.
+  short int numGames;
+  cin >> numGames;
 
-    return 0;
+  for(int i = 0; i < numGames; i++)
+  {
+    short int rowInput, colInput;
+    char** wordSearchMatrix;
+    int solution[4];
+    string word;
+
+    cin >> rowInput;
+    cin >> colInput;
+
+    wordSearchMatrix = build_matrix(rowInput, colInput);
+    fill_matrix(rowInput, colInput, wordSearchMatrix);
+
+    cin >> word;
+
+    matrix_search(solution, word, rowInput, colInput, wordSearchMatrix);
+    
+    cout << "Searching for \"" << word << "\" in matrix " << i << " yields:" << endl;
+    if (solution[0] == -1)
+    {
+      cout << "The pattern was not found" << endl;
+    }
+    else
+    {
+      cout << "Start pos:(" << solution[0] << ", " << solution[1] << ") to End pos:(" << solution[2] << ", " << solution[3] << ")" << endl;
+    }
+    
+    //print_matrix(rowInput, colInput, wordSearchMatrix);
+
+    delete_matrix(rowInput, wordSearchMatrix);
+  }
+
+  
+
+  return 0;
 }
 
